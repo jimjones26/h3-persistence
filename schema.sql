8cf2c8c60fd91de9b687a017488f71127daa7508
jjones <jimmy.jones@tutanota.com>
2021-01-02 12:42:25 -0700

    add tables for scopes

--- schema.sql
+++ schema.sql
@@ -1,7 +1,9 @@
 BEGIN;
 
 -- drop all existing tables
+DROP TABLE IF EXISTS h3.users_scopes;
 DROP TABLE IF EXISTS h3.users;
+DROP TABLE IF EXISTS h3.scopes;
 
 -- drop h3 schema
 DROP SCHEMA IF EXISTS h3;
@@ -23,6 +25,22 @@ CREATE TABLE h3.users (
     updated_at      TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL
 );
 
+-- create the table for scopes
+CREATE TABLE h3.scopes (
+  id              SERIAL PRIMARY KEY,
+  name            VARCHAR (50) NOT NULL,
+  description     VARCHAR NOT NULL,
+  created_at      TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL,
+  updated_at      TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL
+);
+
+-- create the user / scopes junction table
+CREATE TABLE h3.users_scopes (
+  user_id         INT REFERENCES h3.users (id),
+  scope_id        INT REFERENCES h3.scopes (id),
+  PRIMARY KEY     (user_id, scope_id)
+);
+
 -- function to update timestamp
 CREATE FUNCTION set_current_timestamp_updated_at() RETURNS trigger
     LANGUAGE plpgsql
@@ -38,3 +56,6 @@ $$;
 
 -- trigger for updating td.users updated_at
 CREATE TRIGGER set_h3_users_updated_at BEFORE UPDATE ON h3.users FOR EACH ROW EXECUTE PROCEDURE set_current_timestamp_updated_at();
+
+-- trigger for updating td.scopes updated_at
+CREATE TRIGGER set_h3_scopes_updated_at BEFORE UPDATE ON h3.scopes FOR EACH ROW EXECUTE PROCEDURE set_current_timestamp_updated_at();
