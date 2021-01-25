66a873ef354df129f962737b88d14ac75398bd85
jjones <jimmy.jones@tutanota.com>
2021-01-25 10:55:57 -0700

    add table for practitioner with business fields

--- schema.sql
+++ schema.sql
@@ -4,6 +4,7 @@ BEGIN;
 DROP TABLE IF EXISTS h3.users_scopes;
 DROP TABLE IF EXISTS h3.users;
 DROP TABLE IF EXISTS h3.scopes;
+DROP TABLE IF EXISTS h3.practitioners;
 
 -- drop h3 schema
 DROP SCHEMA IF EXISTS h3;
@@ -18,7 +19,7 @@ CREATE SCHEMA IF NOT EXISTS h3;
 CREATE TABLE h3.users (
     id              SERIAL PRIMARY KEY,
     token_version   INT DEFAULT 0,
-    email           VARCHAR (30) NOT NULL UNIQUE,
+    email           VARCHAR(30) NOT NULL UNIQUE,
     first_name      VARCHAR(30),
     last_name       VARCHAR(30),
     phone_number    VARCHAR(10),
@@ -29,20 +30,31 @@ CREATE TABLE h3.users (
     updated_at      TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL
 );
 
+-- create a table for practitioner profile data
+CREATE TABLE h3.practitioners (
+  user_id           INT REFERENCES h3.users (id),
+  business_name     VARCHAR(30),
+  address           VARCHAR(30),
+  city              VARCHAR(30),
+  state             VARCHAR(30),
+  zip               VARCHAR(30),
+  PRIMARY KEY       (user_id)
+);
+
 -- create the table for scopes
 CREATE TABLE h3.scopes (
-  id              SERIAL PRIMARY KEY,
-  name            VARCHAR (50) NOT NULL,
-  description     VARCHAR NOT NULL,
-  created_at      TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL,
-  updated_at      TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL
+  id                SERIAL PRIMARY KEY,
+  name              VARCHAR(50) NOT NULL,
+  description       VARCHAR NOT NULL,
+  created_at        TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL,
+  updated_at        TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL
 );
 
 -- create the user / scopes junction table
 CREATE TABLE h3.users_scopes (
-  user_id         INT REFERENCES h3.users (id),
-  scope_id        INT REFERENCES h3.scopes (id),
-  PRIMARY KEY     (user_id, scope_id)
+  user_id           INT REFERENCES h3.users (id),
+  scope_id          INT REFERENCES h3.scopes (id),
+  PRIMARY KEY       (user_id, scope_id)
 );
 
 -- function to update timestamp
@@ -63,3 +75,6 @@ CREATE TRIGGER set_h3_users_updated_at BEFORE UPDATE ON h3.users FOR EACH ROW EX
 
 -- trigger for updating td.scopes updated_at
 CREATE TRIGGER set_h3_scopes_updated_at BEFORE UPDATE ON h3.scopes FOR EACH ROW EXECUTE PROCEDURE set_current_timestamp_updated_at();
+
+-- trigger for updating td.practitioners updated_at
+CREATE TRIGGER set_h3_practitioners_updated_at BEFORE UPDATE ON h3.practitioners FOR EACH ROW EXECUTE PROCEDURE set_current_timestamp_updated_at();
